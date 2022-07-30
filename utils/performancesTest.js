d23e8e9bc63dd33054bd9d065183376a862a2eb2
laminemessaci <lamine.messaci18@gmail.com>
2022-07-30 15:58:09 +0200

    Added test performance

---
+++ utils/performancesTest.js
@@ -0,0 +1,36 @@
+import { URL_DATA } from "../constants/index.js";
+import { RecipesList } from "../model/recipesList.js";
+import { DataManager } from "../scripts/services/dataManager.js";
+import { getData } from "../scripts/services/getData.js";
+
+const rawRecipesList = await getData(URL_DATA);
+
+const dataManager = new DataManager(rawRecipesList.recipes);
+const recipesList = dataManager.getRecipesList();
+
+export const algorithmPerformance = () => {
+  let RESULT_1, RESULT_2;
+
+  const USER_REQUEST_ONE = {
+    userInput: "coco",
+    joinedBadges: "Ail",
+  };
+  const USER_REQUEST_TOW = {
+    userInput: "coco",
+    joinedBadges: "mais Basilic",
+  };
+
+  const RECIPES_COUNT = 1000;
+  const STARTING_TIME = Date.now();
+
+  for (let _ = 0; _ < RECIPES_COUNT; _++) {
+    RESULT_1 = recipesList.search(USER_REQUEST_ONE);
+    RESULT_2 = recipesList.search(USER_REQUEST_TOW);
+  }
+
+  const ENDING_TIME = Date.now();
+
+  const DURATION = ENDING_TIME - STARTING_TIME;
+
+  console.log(`${2 * RECIPES_COUNT} recherches réalisées en ${DURATION} ms`);
+};
